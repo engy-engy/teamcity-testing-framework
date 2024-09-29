54ef37f8ce56a999e72045ed72a8d8d245fd868a
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-09-29 14:35:46 +0700

    Add template Checked и Unchecked for CRUD endpoints

--- src/main/java/com/example/teamcity/api/enums/Endpoint.java
+++ src/main/java/com/example/teamcity/api/enums/Endpoint.java
@@ -2,6 +2,7 @@ package com.example.teamcity.api.enums;
 
 import com.example.teamcity.api.models.BaseModel;
 import com.example.teamcity.api.models.BuildType;
+import com.example.teamcity.api.models.Project;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,8 +10,9 @@ import lombok.Getter;
 @Getter
 public enum Endpoint {
 
-    BUILD_TYPES("/app/rest/buildTypes", BuildType.class);
-
+    BUILD_TYPES("/app/rest/buildTypes", BuildType.class),
+    PROJECT("/app/rest/projects", Project.class);
     private final String url;
     private final Class<? extends BaseModel> modelClass;
+
 }
---
+++ src/main/java/com/example/teamcity/api/requests/checked/CheckedBase.java
@@ -0,0 +1,52 @@
+package com.example.teamcity.api.requests.checked;
+
+import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.models.BaseModel;
+import com.example.teamcity.api.requests.CrudInterface;
+import com.example.teamcity.api.requests.Request;
+import com.example.teamcity.api.requests.unchecked.UncheckedBase;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+
+@SuppressWarnings("unchecked")
+public final class CheckedBase<T extends BaseModel> extends Request implements CrudInterface {
+
+    private final UncheckedBase unchekedBase;
+
+    public CheckedBase(RequestSpecification spec, Endpoint endpoint) {
+        super(spec, endpoint);
+        this.unchekedBase = new UncheckedBase(spec,endpoint);
+    }
+
+    @Override
+    public T create(BaseModel model) {
+        return (T) unchekedBase
+                .create(model)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+    }
+
+    @Override
+    public T read(String id) {
+        return (T) unchekedBase
+                .read(id)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+    }
+
+    @Override
+    public T update(String id, BaseModel model) {
+        return (T) unchekedBase
+                .update(id, model)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+    }
+
+    @Override
+    public Object delete(String id) {
+        return unchekedBase
+                .read(id)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().asString();
+    }
+}
