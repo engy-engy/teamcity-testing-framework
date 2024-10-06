c93e92232fcdb44e6bfd93b97b0c9a97015a2a52
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-10-06 10:18:40 +0700

    Add two tests for ProjectTest

--- src/test/java/com/example/teamcity/api/ProjectTest.java
+++ src/test/java/com/example/teamcity/api/ProjectTest.java
@@ -3,17 +3,47 @@ package com.example.teamcity.api;
 import com.example.teamcity.api.models.BuildType;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.requests.CheckedRequests;
+import com.example.teamcity.api.requests.UncheckedRequests;
+import com.example.teamcity.api.requests.unchecked.UncheckedBase;
 import com.example.teamcity.api.spec.Specifications;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 import static com.example.teamcity.api.enums.Endpoint.*;
 import static com.example.teamcity.api.enums.Endpoint.BUILD_TYPES;
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 
 @Test(groups = {"Regression"})
 public class ProjectTest extends BaseApiTest{
 
-    @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
-    public void userCreateProjectTest() {
-
+    @Test(description = "User should be able to create project", groups = {"Positive", "CRUD"})
+    public void userSuccessCreateProjectTest() {
+        superUserCheckRequests.getRequest(USERS).create(testData.getUser());
+        var userAuthSpec = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+        var project = userAuthSpec.getRequest(PROJECTS).create(testData.getProject());
+        softy.assertEquals(testData.getProject(), project);
     }
+
+    @Test(description = "User cannot be able to create project same id project", groups = {"Negative", "CRUD"})
+    public void userCannotCreateTwoProjectWithTheSameIdTest() {
+        superUserCheckRequests.getRequest(USERS).create(testData.getUser());
+        var userAuthSpec = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+        userAuthSpec.getRequest(PROJECTS).create(testData.getProject());
+
+        testData.getProject().setId(testData.getProject().getId());
+
+        new UncheckedBase(Specifications.authSpec(testData.getUser()), PROJECTS)
+                .create(testData.getProject())
+                .then()
+                .assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("DuplicateProjectNameException: Project with this name already exists: "
+                        + testData.getProject().getName()
+                        + "\nError occurred while processing this request."
+                        .formatted(testData.getBuildType().getId())));
+    }
+
+
 }
