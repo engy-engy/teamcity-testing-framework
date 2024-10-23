cb3664ed9c2f3ffe7cd4585a369bc8e6fc1ec5b2
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-10-23 11:44:12 +0700

    Added default waiting, checked state API and UI

--- src/test/java/com/example/teamcity/ui/CreateProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateProjectTest.java
@@ -1,9 +1,14 @@
 package com.example.teamcity.ui;
 
+import com.codeborne.selenide.Condition;
+import com.example.teamcity.api.models.Project;
 import com.example.teamcity.ui.pages.admin.CreateProjectPage;
+import com.example.teamcity.ui.pages.admin.ProjectPage;
 import org.testng.annotations.Test;
 
+import static com.example.teamcity.api.enums.Endpoint.PROJECTS;
 import static io.qameta.allure.Allure.step;
+
 @Test(groups = {"Regression"})
 public class CreateProjectTest extends BaseUiTest {
 
@@ -16,22 +21,20 @@ public class CreateProjectTest extends BaseUiTest {
         loginAs(testData.getUser());
 
         // Взаимодействие с UI ()
-        step("Open Create Project Page (http://localhost:8111/admin/createObjectMenu.html)");
-        step("Send all project parameters (repository URL)");
-        step("Click Proceed");
-        step("Fix Project name and Build Type name values");
-        step("Click Proceed");
         CreateProjectPage.open("_Root")
                 .createForm(REPO_URL)
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
-
-
         // Проверка состояния API (корректность отправки данных с клиента на API)
         step("Check that all entities (project, buildType) was successfully created with correct data on API level");
+        var createdProject = superUserCheckRequests.<Project>getRequest(PROJECTS).read("name:" + testData.getProject().getName());
+        softy.assertNotNull(createdProject);
 
         // Проверка состояния UI (корректность считывания данных и отображение данных на UI)
         step("Check that project is visible on Project Page (http://localhost:8111/favorite/projects)");
+        ProjectPage.open(createdProject.getId())
+                .title.shouldHave(Condition.exactText(testData.getProject().getName()));
+
     }
 
     @Test(description = "User should not be able to create project without name", groups = {"Negative"})
