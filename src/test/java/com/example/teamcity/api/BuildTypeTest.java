5ea0f64f3caed473818d9bcb0a397065ad72c040
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-11-17 20:47:04 +0700

    Run tests 45

--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -51,6 +51,7 @@ public class BuildTypeTest extends BaseApiTest {
 
         userCheckRequests.getRequest(BUILD_TYPES).create(testData.getBuildType());
         userCheckRequests.<BuildType>getRequest(BUILD_TYPES).read("id:" + testData.getBuildType().getId());
+
         generate(BuildQueue.class);
         testData.getBuildQueue().getBuildType().setId(testData.getBuildType().getId());
 
