ce71ffefed2b56f609bf6f3ff8ad4eb9bdeab096
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-11-20 11:19:02 +0700

    Fix Run Teamcity Agent 3

--- src/test/java/com/example/teamcity/api/AuthAgentTest.java
+++ src/test/java/com/example/teamcity/api/AuthAgentTest.java
@@ -12,7 +12,29 @@ import static com.example.teamcity.api.enums.Endpoint.*;
 public class AuthAgentTest extends BaseApiTest {
     @Test(groups = {"Setup"})
     public void authAgentTest() {
-        superUserCheckRequests.getRequest(USERS).create(testData.getUser());
+
+        Response listAgentsPool =  new UncheckedBase(Specifications.superUserSpec(), AGENTS_POOL)
+                .read("")
+                .then()
+                .assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+        var agentPoolId = listAgentsPool.jsonPath().getString("agentPool[0].id");
+
+        Response responseAgent =  new UncheckedBase(Specifications.superUserSpec(), AGENTS_POOL)
+                .read("id:" + agentPoolId)
+                .then()
+                .assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+        var agentId2 = responseAgent.jsonPath().getString("agents.agent[0].id");
+        testData.getAuthorizedInfo().setStatus(true);
+
+        Response response =  new UncheckedBase(Specifications.superUserSpec(), AGENTS)
+                .update("id:" + agentId2 + "/authorizedInfo", testData.getAuthorizedInfo())
+                .then()
+                .assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+        softy.assertTrue(response.jsonPath().getBoolean("status"), "Status not true");
+        /*
 
         Response agents =  new UncheckedBase(Specifications.superUserSpec(), AGENTS_POOL)
                 .read("name:Default")
@@ -28,6 +50,6 @@ public class AuthAgentTest extends BaseApiTest {
                 .then()
                 .assertThat().statusCode(HttpStatus.SC_OK)
                 .extract().response();
-        softy.assertTrue(response.jsonPath().getBoolean("status"), "Status not true");
+        softy.assertTrue(response.jsonPath().getBoolean("status"), "Status not true");*/
     }
 }
