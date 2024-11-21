2e7bf4ebc0406fcc71a832eb6dacad1dfd92460a
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-11-21 10:21:06 +0700

    Try auth teamcity agent

--- src/test/java/com/example/teamcity/api/AuthAgentTest.java
+++ src/test/java/com/example/teamcity/api/AuthAgentTest.java
@@ -6,12 +6,26 @@ import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.teamcity.api.enums.Endpoint.*;
+import static org.awaitility.Awaitility.await;
 
 public class AuthAgentTest extends BaseApiTest {
     @Test(groups = {"Setup"})
     public void authAgentTest() {
         testData.getAuthorizedInfo().setStatus(true);
+
+        await().atMost(60, TimeUnit.SECONDS).until(() -> {
+            Response checkAgentsResponse = new UncheckedBase(Specifications.superUserSpec(), AGENTS)
+                    .read("?locator=authorized:any")
+                    .then()
+                    .assertThat().statusCode(HttpStatus.SC_OK)
+                    .extract().response();
+
+            String currentAgentId = checkAgentsResponse.jsonPath().getString("agent[0].id");
+            return currentAgentId != null && !currentAgentId.isEmpty();
+        });
 
         Response listAgentsPool = new UncheckedBase(Specifications.superUserSpec(), AGENTS)
                 .read("?locator=authorized:any")
