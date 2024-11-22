01ce0aaafa84fd988231fcacbf2eedd8b09ef81d
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-11-22 20:18:34 +0700

    Run tests

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,25 +1,17 @@
 package com.example.teamcity;
 
-import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.generators.TestDataStorage;
 import com.example.teamcity.api.models.TestData;
 import com.example.teamcity.api.requests.CheckedRequests;
 import com.example.teamcity.api.requests.UncheckedRequests;
 import com.example.teamcity.api.spec.Specifications;
-import com.example.teamcity.ui.BaseUiTest;
-import io.qameta.allure.Allure;
 import org.assertj.core.api.SoftAssertions;
-import org.testng.IHookCallBack;
-import org.testng.IHookable;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.asserts.SoftAssert;
 
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
-import static io.qameta.allure.util.ResultsUtils.TAG_LABEL_NAME;
 
-public class BaseTest implements IHookable {
+public class BaseTest {
 
     protected CheckedRequests superUserCheckRequests = new CheckedRequests(Specifications.superUserSpec());
     protected UncheckedRequests uncheckedSuperUser = new UncheckedRequests(Specifications.superUserSpec());
@@ -43,19 +35,6 @@ public class BaseTest implements IHookable {
     public void afterTest() {
         softy.assertAll();
         TestDataStorage.getStorage().deleteCreatedEntities();
-    }
-
-    @Override
-    public void run(IHookCallBack callBack, ITestResult testResult) {
-        softy = new SoftAssertions();
-        // Добавляем сьют и тэг для лучшей информативности и возможности фильтрации тестов в Allure репорте
-        if (BaseUiTest.class.isAssignableFrom(testResult.getTestClass().getRealClass())) {
-            var browser = Config.getProperty("browser");
-            Allure.suite(browser);
-            Allure.label(TAG_LABEL_NAME, browser);
-        }
-        callBack.runTestMethod(testResult);
-        softy.assertAll();
     }
 
 }
